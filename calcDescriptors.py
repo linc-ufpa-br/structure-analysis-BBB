39d62594a1b76c6b10ba633f4b089a243c792435
Juliana Auzier Seixas Feio <juliana.auzier.s@gmail.com>
2022-12-23 16:59:04 -0300

    function calc all descriptors - mordred

--- calcDescriptors.py
+++ calcDescriptors.py
@@ -5,24 +5,20 @@ from rdkit import Chem
 import filter
 from calcRdkit import calc
 from calcMordred import calcSpecific
+from calcMordred import calcAllDescriptors
 
 # data for test
 basePDB = os.getcwd() + '/data/1pef.pdb'
 baseMol = os.getcwd() + '/data/mol_files/BBB+/mol_18.mol'
 baseCSV = os.getcwd() + '/data/brainPeps.csv'
 
-# descriptors calculated in dichiara dataset + weight
+# descriptors calculated in dichiara dataset
 descriptors = ['weight','logP', 'TPSA(Tot)', 'HBA', 'HBD', 'nN', 'nO', 'n(N+O)']
 
-# folder to save Rdkit Results
-if not os.path.exists("RdkitResults"):
-    os.makedirs("RdkitResults")
-
-# folder to save Mordred Results
-if not os.path.exists("MordredResults"):
-    os.makedirs("MordredResults")
-
 def descriptorsRdkit(data, colPeptides ='peptides', colKeep = None):
+    if not os.path.exists("RdkitResults"):
+        os.makedirs("RdkitResults")
+
     ext = (os.path.basename(data)).split('.')[-1]
 
     # for pdb files
@@ -53,7 +49,7 @@ def descriptorsRdkit(data, colPeptides ='peptides', colKeep = None):
         results = []
 
         for i in range(len(molCSV)):
-            # removing break lines (!don't always need!)
+            # removing break lines (don't always need)
             molCSV.iloc[i][colPeptides] = str(molCSV.iloc[i][colPeptides]).strip()
 
         # only natural peptide
@@ -74,6 +70,9 @@ def descriptorsRdkit(data, colPeptides ='peptides', colKeep = None):
         return 'Unsupported file type.'
 
 def descriptorsMordred(data, colPeptides ='peptides', colKeep = None):
+    if not os.path.exists("MordredResults"):
+        os.makedirs("MordredResults")
+
     ext = (os.path.basename(data)).split('.')[-1]
 
     # for pdb files
@@ -104,7 +103,7 @@ def descriptorsMordred(data, colPeptides ='peptides', colKeep = None):
         results = []
 
         for i in range(len(molCSV)):
-            # removing break lines (!don't always need!)
+            # removing break lines (don't always need)
             molCSV.iloc[i][colPeptides] = str(molCSV.iloc[i][colPeptides]).strip()
 
         # only natural peptide
@@ -124,6 +123,55 @@ def descriptorsMordred(data, colPeptides ='peptides', colKeep = None):
     else:
         return 'Unsupported file type.'
 
+def allDescriptorsMordred(data, colPeptides ='peptides', colKeep = None):
+    if not os.path.exists("allMordredResults"):
+        os.makedirs("allMordredResults")
+
+    ext = (os.path.basename(data)).split('.')[-1]
+
+    # for pdb files
+    if ext == 'pdb':
+        chain = {record.id: record.seq for record in SeqIO.parse(data, 'pdb-seqres')}
+        for k, v in chain.items():
+            seqPDB = v
+            naturalPep = filter.naturalPep(seqPDB)
+
+        if naturalPep is not False:
+            molPDB = Chem.MolFromPDBFile(data)
+            PDBresult = pd.DataFrame(calcAllDescriptors(molPDB))
+            return PDBresult.to_csv('allMordredResults/PDBresult.csv', index=False)
+        else:
+            print("Error: Not natural peptide!")
+
+    # for mol files (cannot extract sequence for filter)
+    elif ext == 'mol':
+        molMol = Chem.MolFromMolFile(data)
+        MOLresult = pd.DataFrame(calcAllDescriptors(molMol))
+        return MOLresult.to_csv('allMordredResults/MOLresult.csv', index=False)
+
+    # for csv files
+    elif ext == 'csv':
+        molCSV = pd.read_csv(data)
+
+        for i in range(len(molCSV)):
+        # removing break lines (don't always need)
+            molCSV.iloc[i][colPeptides] = str(molCSV.iloc[i][colPeptides]).strip()
+
+        # only natural peptide
+        naturalmolCSV = []
+        for x, y in zip(molCSV[colPeptides], molCSV[colKeep]):
+            if filter.naturalPep(x) is not None:
+                naturalmolCSV.append([x, y])
+        naturalmolCSV = pd.DataFrame(naturalmolCSV)
+
+        results = pd.DataFrame([])
+        for i in range(len(naturalmolCSV)):
+            calc = calcAllDescriptors(Chem.MolFromSequence(naturalmolCSV[0][i]))
+            results = pd.concat([results, calc], axis=0, ignore_index=True)
+
+        naturalmolCSV.columns = [colPeptides] + [colKeep]
+        CSVresult = pd.concat([naturalmolCSV, pd.DataFrame(results)], axis=1)
+        CSVresult.to_csv('allMordredResults/CSVresult.csv', index=False)
 
 if __name__ == '__main__':
     descriptorsRdkit(baseMol)
@@ -133,3 +181,7 @@ if __name__ == '__main__':
     descriptorsMordred(baseMol)
     descriptorsMordred(basePDB)
     descriptorsMordred(baseCSV, 'onelettersequence', 'label')
+
+    allDescriptorsMordred(baseMol)
+    allDescriptorsMordred(basePDB)
+    allDescriptorsMordred(baseCSV, 'onelettersequence', 'label')
